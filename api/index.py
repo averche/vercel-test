3374748b07a3e2a32d464383f503a701f8e46a32
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-06-20 12:44:38 -0400

    test env

--- api/index.py
+++ api/index.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return f'Hello, TEST_ENV = {os.environ['TEST_ENV']}'
 
 @app.route('/about')
 def about():
